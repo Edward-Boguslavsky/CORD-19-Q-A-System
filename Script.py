024261c3402e2d9cdd28feef91c82979d314c942
HFuz <nonfloodedgm@gmail.com>
2024-02-04 21:39:16 -0500

    A1 Final
    Includes jsons

--- Script.py
+++ Script.py
@@ -1,92 +1,133 @@
+import time
 import spacy
 import glob
-from math import log
 import json, os
-import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+from math import log
+from nltk import bigrams
 from collections import Counter
+from wordcloud import WordCloud
 
+#Timer to help with understanding run time
+startTime = time.time()
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_md')
 
+# Gives a list of all the json files in any folder under document_parses
 docPath = glob.glob(os.path.join('document_parses','*','*.json'))
-outPath = 'Output'
-os.makedirs(outPath, exist_ok=True)
 
-numDocs = 0
-count = 0
+# Will make an Output folder if needed
+outFolder = 'Output'
+os.makedirs(outFolder, exist_ok=True)
+
+# Words from languages we don't use but spaCy still includes
+bannedWords = ['et', 'de', 'al', 'la']
+
+# Number of documents to read and that have been read
+numDocs = 1000
+numRead = 0
+
+# Initialize counters and lists we will use later
+vocabulary = []
 docFreq = Counter()
-vocabulary = Counter() 
 entities = Counter()
-numEntities = Counter()
+allEntities = Counter()
+termFrequency = Counter()
+bigramCounter = Counter()
 
-
+# Iterate through the list of files specified earlier
 for filename in docPath:
-    count += 1;
     
+    # Open current File 
     with open(filename, 'r', encoding='utf-8') as file:
-
+        
+        # Load any 'text' headers under 'body_text' as raw text
         data = json.load(file)
-        texts = (entry['text'] for entry in data['body_text'])
-        # disable=["parser", "tagger"],
-        for doc in nlp.pipe(texts, batch_size=10):
-            numDocs += 1
+        texts = ''.join((entry['text'] for entry in data['body_text']))
+        
+        # Create a spaCy doc out of our raw text and extract useful tokens
+        doc = nlp(texts)
+        tokens = [token.text for token in doc if not (token.is_stop or token.is_punct or token.is_space)]
+        
+        # Omit any documents that contain banned words as tokens
+        if any(word in tokens for word in bannedWords):
+            continue
+        
+        # Record useful data
+        entryBigrams = list(bigrams(tokens))
+        bigramCounter.update(entryBigrams)
+        termFrequency.update(tokens)
+        entities = [ent.text for ent in doc.ents]
+        allEntities.update(entities)
             
-            tokens = [token.text for token in doc if not token.is_stop and not token.is_punct and not token.is_space]
-            vocabulary.update(tokens)
-            docFreq.update(set(tokens))
-            entities = [ent.text for ent in doc.ents]
-            numEntities.update(entities)
-            
-            
-    if (count > 10):
+    docFreq.update(set(tokens))
+    
+    # For keeping track of progress and ending when we've read enough documents
+    print( str(100* numRead / numDocs) + "%")
+    
+    numRead += 1
+    if (numRead > numDocs):
         break
-IDF = {word: log(numDocs / freq, 10) for word, freq in docFreq.items()}
 
-print(vocabulary)
+# Round Term Frequencies
+for term in termFrequency:
+    termFrequency[term] = round(termFrequency[term] / numRead , 3)
 
-'''
-allJson = []
+# Generate vocabulary using the keys from term frequency
+vocabulary = list(dict.fromkeys(termFrequency))
 
-docPath = glob.glob(os.path.join('document_parses','*','*.json'))
+# Inverse document frequency is calculated
+IDF = Counter({word: round(log(numRead / freq, 10),3) for word, freq in docFreq.items()})
 
-for filename in docPath:
-    
-    with open(filename, 'r') as inFile:
-            allJson.extend(json.load(inFile))
+# Draw Barchart for IDF
+topIDF = IDF.most_common()[:-30-1:-1]
+IDFWords = [item[0] for item in topIDF]
+IDFfrequencies = [item[1] for item in topIDF]
 
-with open('merged.json', 'w') as outFile:
-        json.dump(allJson, outFile)
+plt.figure(figsize=(10, 8))
+plt.barh(IDFWords, IDFfrequencies)
+plt.xlabel('Inverse Document Frequency')
+plt.title('Top 30 Most Common Words')
+plt.gca().invert_yaxis()  
+plt.show()
 
-data = json.load(open('document_parses/pdf_json/0000b6da665726420ab8ac9246d526f2f44d5943.json', 'rb'))
+# Draw Barchart for Term Frequency
+topFreq = termFrequency.most_common(30)
+freqWords = [item[0] for item in topFreq]
+frequencies = [item[1] for item in topFreq]
 
-for par in range(len(data['body_text'])):
-    print(data['body_text'][par]['text'])
-    
-for par in range(len(data['abstract'])):
-    print(data['abstract'][par]['text'])
+plt.figure(figsize=(10, 8))
+plt.barh(freqWords, frequencies)
+plt.xlabel('Frequencies')
+plt.title('Top 30 Most Frequent Words')
+plt.gca().invert_yaxis()  
+plt.show()
 
+# Get the most common bigrams and put them in a dictionary
+bigramFrequencies = bigramCounter.most_common(50)
+bigramDict = {'<>'.join(bigram): frequency for bigram, frequency in bigramFrequencies}
 
-outFile = open('rawText.txt', 'a', encoding='utf-8')
+# Create and generate a word cloud image
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(bigramDict)
 
-for i, filename in enumerate(docPath):
-    
-    data = json.load(open(filename, 'r'))
-    
-    for par in range(len(data['body_text'])):
-        outFile.write(data['body_text'][par]['text'] + ' \n')
-    
-    for par in range(len(data['abstract'])):
-        outFile.write(data['abstract'][par]['text']  + ' \n')
-    
-    print(filename, 100 * i/716954)
-    
-outFile.close()
+# Display the generated word cloud image
+plt.figure(figsize=(15, 7))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')  # Remove axis
+plt.show()
 
-with open('merged.json', 'w') as outFile:
-        json.dump(allJson, outFile)
+# Write Results to the output folder
+with open(os.path.join(outFolder, 'vocabulary.json'), 'w') as vocab_file:
+    json.dump(vocabulary, vocab_file)
+with open(os.path.join(outFolder, 'entities.json'), 'w') as entity_file:
+    json.dump(allEntities, entity_file)
+with open(os.path.join(outFolder, 'term_frequency.json'), 'w') as tf_file:
+    json.dump(termFrequency, tf_file)
+with open(os.path.join(outFolder, 'idf.json'), 'w') as idf_file:
+    json.dump(IDF, idf_file)
+with open(os.path.join(outFolder, 'bigram.json'), 'w') as bigram_file:
+    json.dump(str(bigramCounter), bigram_file)
 
-#metaData = pd.read_csv('metadata.csv')
-#print(metaData)
+print("--- %s seconds ---" % round((time.time() - startTime),3))
 
-'''
+print('Done')
